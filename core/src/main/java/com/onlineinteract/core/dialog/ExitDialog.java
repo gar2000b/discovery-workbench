5fe6a9140c1180300925b578e4988dd6b9da13c1
Gary Black <gar2000b@yahoo.com>
2016-11-04 23:50:51 +0100

    Proving that we can set the input processor back to DeviceInputProcessor on dialog exit.

--- core/src/main/java/com/onlineinteract/core/dialog/ExitDialog.java
+++ core/src/main/java/com/onlineinteract/core/dialog/ExitDialog.java
@@ -1,16 +1,21 @@
 package com.onlineinteract.core.dialog;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.scenes.scene2d.ui.Dialog;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.onlineinteract.core.Workspace;
 
 public class ExitDialog extends Dialog {
+
+	private Workspace workspace;
 
 	public ExitDialog(String title, Skin skin, String windowStyleName) {
 		super(title, skin, windowStyleName);
 	}
 
-	public ExitDialog(String title, Skin skin) {
+	public ExitDialog(String title, Skin skin, Workspace workspace) {
 		super(title, skin);
+		this.workspace = workspace;
 	}
 
 	public ExitDialog(String title, WindowStyle windowStyle) {
@@ -26,5 +31,6 @@ public class ExitDialog extends Dialog {
 	@Override
 	protected void result(Object object) {
 		System.out.println("Some result here");
+		Gdx.input.setInputProcessor(workspace.getDeviceInputProcessor());
 	}
 }
